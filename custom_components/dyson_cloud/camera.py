71815af6d75ee40637515a9e54f39aa4e05b8aa2
Ben Vezzani <dotvezz@gmail.com>
2023-06-08 09:43:16 -0400

    Vendoring libdyson to avoid conflicts and other dependency install errors

--- custom_components/dyson_cloud/camera.py
+++ custom_components/dyson_cloud/camera.py
@@ -4,9 +4,9 @@ import logging
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.components.camera import Camera
-from libdyson.const import DEVICE_TYPE_360_EYE, DEVICE_TYPE_360_HEURIST
-from libdyson.cloud.cloud_360_eye import DysonCloud360Eye
-from libdyson.cloud import DysonDeviceInfo
+from .vendor.libdyson.const import DEVICE_TYPE_360_EYE, DEVICE_TYPE_360_HEURIST
+from .vendor.libdyson.cloud.cloud_360_eye import DysonCloud360Eye
+from .vendor.libdyson.cloud import DysonDeviceInfo
 from datetime import timedelta
 
 from .const import DATA_ACCOUNT, DATA_DEVICES, DOMAIN
